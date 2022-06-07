e761018a62e9d9f6562898f2d87917fb1173a432
JCB1980 <97905082+JCB1980@users.noreply.github.com>
2022-06-07 14:39:55 +0200

    Update TabAmorMobilier.vue

--- src/components/TabAmorMobilier.vue
+++ src/components/TabAmorMobilier.vue
@@ -1,10 +1,11 @@
 <template>
   <div>
-
-    <span>Mon tableau d'amortissement  : {{ lmnp.annuite }}</span>
-
+    <span>Valeur nette comptable année N : {{ lmnp.amortissementMobilier.vnc }}</span>
+    <br />
+    <span>Annuité N : {{ lmnp.amortissementMobilier.annuite }}</span>
+    <br />
+    <span>Date de début d'activité : {{ lmnp.dateDebut.startDate }}</span>
   </div>
-
 </template>
 
 <script setup lang="ts">
